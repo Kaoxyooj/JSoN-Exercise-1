e2d46d1a4d1ab15568b0eb09f7427157e52a5657
Kaoxyooj <locksage@hotmail.com>
2016-02-05 16:53:43 -0600

    Serialize and Deserialize exercise

---
+++ JSoN Exercise/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JSoN_Exercise
+{
+    class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Name: {0} \nAge: {1}", name, age);
+        }
+    }
+}
---
+++ JSoN Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JSoN_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String JSONstring = File.ReadAllText("json1.json");
+
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            Person p1 = ser.Deserialize<Person>(JSONstring);
+            Console.WriteLine(p1);
+
+
+
+            Person p2 = new Person() { name = "Kong", age = 31 };
+            string outputJson = ser.Serialize(p2);
+            File.WriteAllText("json1.json", outputJson);
+
+            Console.Read();
+        }
+    }
+}
